c03382599ab6123ca04bf9a1111483e0f080f61d
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-02-19 17:11:23 +0100

    Fix nakd_log segfault
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- log.c
+++ log.c
@@ -4,15 +4,29 @@
 #include <syslog.h>
 #include "log.h"
 
+#define CRIT    "CRITICAL"
+#define WARNING "WARNING"
+#define NOTICE  "NOTICE"
+#define INFO    "INFO"
+#define DEBUG   "DEBUG"
+
 static int use_syslog = 1;
 static int loglevel = DEFAULT_LOG_LEVEL;
 
-static const int log_class[] = {
+static int *syslog_loglevel[] = {
     [L_CRIT] = LOG_CRIT,
     [L_WARNING] = LOG_WARNING,
     [L_NOTICE] = LOG_NOTICE,
     [L_INFO] = LOG_INFO,
     [L_DEBUG] = LOG_DEBUG
+};
+
+static const char *loglevel_string[] = {
+    [L_CRIT] = CRIT,
+    [L_WARNING] = WARNING,
+    [L_NOTICE] = NOTICE,
+    [L_INFO] = INFO,
+    [L_DEBUG] = DEBUG
 };
 
 void nakd_set_loglevel(int level) {
@@ -39,9 +53,9 @@ void nakd_log(int priority, const char *format, ...) {
 
     va_start(vl, format);
     if (use_syslog) {
-        vsyslog(log_class[priority], format, vl);
+        vsyslog(syslog_loglevel[priority], format, vl);
     } else {
-        fprintf(stderr, "[%s] ", log_class[priority]);
+        fprintf(stderr, "[%s] ", loglevel_string[priority]);
         vfprintf(stderr, format, vl);
         fprintf(stderr, "\n");
     }

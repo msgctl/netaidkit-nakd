msgctl/netaidkit-nakd

commit 0f2327390b05c5cf4f7a7bef72f94ddcff5b8446

Use JSON in daemon-client communication
A way to pass aggregate types of arbitrary size was needed.

JSON fits the requirements as it's ubiquitous in OpenWRT environment, already
being used by ubus [1].

[1] https://wiki.openwrt.org/doc/techref/ubus

Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

command.c
@@ -1,41 +1,49 @@
-#include <wifi.h>
-#include <stage.h>
-#include <inet.h>
-#include <update.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <command.h>
-
-#define N_COMMANDS  (sizeof(commands) / sizeof(commands[0]))
+#include "command.h"
+#include "misc.h"
+#include "shell.h"
+#include "log.h"
 
 static command commands[] = {
-    { "wifiscan", scan_wifi, 0 },
-    { "apconfig", ap_config, 2 },
-    { "getapnam", get_ap_name, 0 },
-    { "wificonn", connect_wifi, 2 },
-    { "goonline", go_online, 0 },
-    { "inetstat", inet_stat, 0 },
-    { "nrouting", toggle_routing, 1},
-    { "wlaninfo", wlan_info, 1},
-    { "setstage", set_stage, 1 },
-    { "getstage", get_stage, 0 },
-    { "stagetor", toggle_tor, 1 },
-    { "stagevpn", toggle_vpn, 1 },
-    { "doupdate", do_update, 1},
-    { "broadcst", toggle_broadcast, 1},
-    { "isportal", detect_portal, 0}
+//    { "getapnam", get_ap_name, 0 },
+    { "wifiscan", cmd_shell,   "/nak/scripts/iwinfo.sh"        },
+    { "apconfig", cmd_shell,   "/nak/scripts/setup_ap.sh"      },
+    { "wificonn", cmd_shell,   "/nak/scripts/setup_wan.sh"     },
+    { "goonline", cmd_shell,   "/nak/scripts/go_online.sh"     },
+    { "inetstat", cmd_shell,   "/nak/scripts/get_inetstat.sh"  },
+    { "nrouting", cmd_shell,   "/nak/scripts/toggle_routing.sh"},
+    { "wlaninfo", cmd_shell,   "/nak/scripts/wlan_info.sh"     },
+    { "setstage", cmd_shell,   "/nak/scripts/set_stage.sh"     },
+    { "getstage", cmd_shell,   "/nak/scripts/get_stage.sh"     },
+    { "stagetor", cmd_shell,   "/nak/scripts/toggle_tor.sh"    },
+    { "stagevpn", cmd_shell,   "/nak/scripts/toggle_vpn.sh"    },
+    { "doupdate", cmd_shell,   "/nak/scripts/do_update.sh"     },
+    { "broadcst", cmd_shell,   "/nak/scripts/toggle_broadcast.sh"},
+    { "isportal", cmd_shell,   "/nak/scripts/detect_portal.sh"   }
 };
 
-command *get_command(char *cmd_name) {
+command *nakd_get_command(const char *cmd_name) {
     command *cmd = NULL;
-
     int i;
-    for (i = 0; i < N_COMMANDS; i++) {
-        if ((strncmp(cmd_name, commands[i].name, 8)) == 0) {
+
+    for (i = 0; i < N_ELEMENTS(commands); i++) {
+        if ((strcmp(cmd_name, commands[i].name)) == 0) {
             cmd = &commands[i];
             break;
         }
     }
 
     return cmd;
+}
+
+json_object *nakd_call_command(const char *cmd_name, json_object *jcmd) {
+    command *cmd = nakd_get_command(cmd_name);
+
+    if (cmd == NULL) {
+        nakd_log(L_NOTICE, "Couldn't find command %s.", cmd_name);
+        return NULL;
+    }
+
+    return cmd->handler(jcmd, cmd->priv);
 }

inc/shell.h
@@ -1,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <json-c/json.h>
+
+#define MAX_SHELL_RESULT_LEN 4096
+
+char *nakd_do_command(const char *script, char **argv);
+json_object *nakd_json_do_command(const char *script, json_object *jcmd);
+
+json_object *cmd_shell(json_object *jcmd, void *priv);
+
+#endif

connection.c
@@ -1,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/un.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include "connection.h"
+#include "message.h"
+#include "log.h"
+
+int nakd_handle_connection(int sock) {
+    struct sockaddr_un client_addr;
+    socklen_t client_len = sizeof(struct sockaddr_un);
+    char message_buf[MAX_MSG_LEN];
+    int nb_read = 0, nb_sent;
+    json_tokener *jtok;
+    int nb_parse, parse_offset;
+    json_object *jmsg = NULL;
+    json_object *jresponse = NULL;
+    const char *jrstr;
+    int nb_resp;
+    enum json_tokener_error jerr;
+    int rval = 0;
+
+    jtok = json_tokener_new();
+
+    /* TODO: GRAB CREDENTIALS OF CONNECTING PROCESS */
+
+    for (;;) {
+        do {
+            /* If there was another JSON string in the middle of the buffer,
+             * start with an offset and don't call recvfrom() just yet.
+             */
+            parse_offset = jtok->char_offset < nb_read ? jtok->char_offset : 0;
+            if (!parse_offset) {
+                if ((nb_read = recvfrom(sock, message_buf, MAX_MSG_LEN, 0,
+                          (struct sockaddr *) &client_addr, &client_len)) < 0) { 
+                    nakd_log(L_DEBUG, "recvfrom() < 0, closing connection.");
+                    goto ret;
+                }
+                /* remaining bytes to parse */
+                nb_parse = nb_read;
+            } else {
+                nb_parse = nb_read - jtok->char_offset;
+            }
+
+            /* partial JSON strings are stored in tokener context */
+            jmsg = json_tokener_parse_ex(jtok, message_buf + parse_offset,
+                                                               nb_parse);
+        } while ((jerr = json_tokener_get_error(jtok)) == json_tokener_continue);
+
+        if (jerr == json_tokener_success) {
+            jresponse = nakd_handle_message(jmsg);
+            jrstr = json_object_get_string(jresponse);
+
+            while (nb_resp = strlen(jrstr)) {
+                nb_sent = sendto(sock, jrstr, nb_resp, 0,
+                        (struct sockaddr *) &client_addr,
+                                             client_len);
+                if (nb_sent < 0) {
+                    nakd_log(L_NOTICE,
+                        "Couldn't send response, closing connection.");
+                    rval = 1;
+                    goto ret;
+                }
+                jrstr += nb_sent;
+            }
+
+            json_object_put(jresponse), jresponse = NULL;
+        } else {
+            nakd_log(L_NOTICE, "Couldn't parse client JSON message: %s."
+                                                 " Closing connection.",
+                                         json_tokener_error_desc(jerr));
+            rval = 1;
+            goto ret;
+        }
+
+        json_object_put(jmsg), jmsg = NULL;
+    }
+
+ret:
+    /* NULL-safe functions */
+    json_object_put(jresponse);
+    json_object_put(jmsg);
+    json_tokener_free(jtok);
+    return rval;
+}
+

inc/command.h
@@ -1,17 +1,16 @@
 #ifndef COMMAND_H
 #define COMMAND_H
+#include <json-c/json.h>
 
-/* Length of the name of a command is static. */
-#define CMD_NAME_LEN     8
-
-typedef char* (*cmd_handler)(char **args);
+typedef json_object *(*cmd_handler)(json_object *jcmd, void *priv);
 
 typedef struct {
-    char name[CMD_NAME_LEN];
+    char *name;
     cmd_handler handler;
-    int arg_count;
+    void *priv;
 } command;
 
-command *get_command(char *cmd_name);
+command *nakd_get_command(const char *cmd_name);
+json_object *nakd_call_command(const char *cmd_name, json_object *jcmd);
 
 #endif

inc/connection.h
@@ -1,0 +1,9 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+/* Max size of command sent over domain socket. */
+#define MAX_MSG_LEN     4096
+
+int nakd_handle_connection(int sock);
+
+#endif

inc/inet.h
@@ -1,4 +1,0 @@
-char *go_online(char **args);
-char *inet_stat(char **args);
-char *toggle_routing(char **args);
-char *detect_portal(char **args);

inc/log.h
@@ -1,6 +1,6 @@
 #ifndef LOG_H
 #define LOG_H
-#define DEFAULT_LOG_LEVEL L_NOTICE
+#define DEFAULT_LOG_LEVEL L_DEBUG
 
 enum {
     L_CRIT,

inc/message.h
@@ -1,14 +1,6 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
-
-#include <command.h>
-#include <stdlib.h>
-
-/* Max size of command sent over domain socket. */
-#define MAX_MSG_LEN     262144
-
-/* Max number of arguments passed along with a command. */
-#define MAX_MSG_ARG     8
+#include <json-c/json.h>
 
 typedef enum {
     MSG_TYPE_UNKNOWN,
@@ -22,15 +14,12 @@ typedef enum {
     MSG_STATUS_ERROR
 } msg_status;
 
-typedef struct {
-    char payload[MAX_MSG_LEN + 1];
-    char **args; /* must end with NULL */
-    msg_type type;
-    msg_status status;
-    command *cmd;
-} message;
-
-message *parse_message(msg_type type, char *data);
-char **parse_args(char *data);
+const char *nakd_message_type_str(msg_type type);
+const char *nakd_message_status_str(msg_status status);
+void nakd_message_set_type(json_object *jmsg, msg_type type);
+msg_type nakd_message_type(const char *typestr);
+void nakd_message_set_status(json_object *jmsg, msg_status status);
+msg_status nakd_message_status(const char *statusstr);
+json_object *nakd_handle_message(json_object *jmsg);
 
 #endif

commit 8db8f8983c6c3e17b1b46def40ea3750dc2c0e2a

Fix iwinfo arguments
Bug introduced in 0f23273.

Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

command.c
@@ -7,20 +7,20 @@
 
 static command commands[] = {
 //    { "getapnam", get_ap_name, 0 },
-    { "wifiscan", cmd_shell,   NAKD_SCRIPT("iwinfo.sh")        },
-    { "apconfig", cmd_shell,   NAKD_SCRIPT("setup_ap.sh")      },
-    { "wificonn", cmd_shell,   NAKD_SCRIPT("setup_wan.sh")     },
-    { "goonline", cmd_shell,   NAKD_SCRIPT("go_online.sh")     },
-    { "inetstat", cmd_shell,   NAKD_SCRIPT("get_inetstat.sh")  },
-    { "nrouting", cmd_shell,   NAKD_SCRIPT("toggle_routing.sh")},
-    { "wlaninfo", cmd_shell,   NAKD_SCRIPT("wlan_info.sh")     },
-    { "setstage", cmd_shell,   NAKD_SCRIPT("set_stage.sh")     },
-    { "getstage", cmd_shell,   NAKD_SCRIPT("get_stage.sh")     },
-    { "stagetor", cmd_shell,   NAKD_SCRIPT("toggle_tor.sh")    },
-    { "stagevpn", cmd_shell,   NAKD_SCRIPT("toggle_vpn.sh")    },
-    { "doupdate", cmd_shell,   NAKD_SCRIPT("do_update.sh")     },
-    { "broadcst", cmd_shell,   NAKD_SCRIPT("toggle_broadcast.sh")},
-    { "isportal", cmd_shell,   NAKD_SCRIPT("detect_portal.sh")   }
+    CMD_SHELL_NAKD_ARGV("wifiscan", "iwinfo.sh", "wlan0", "scan"),
+    CMD_SHELL_NAKD("apconfig", "setup_ap.sh"),
+    CMD_SHELL_NAKD("wificonn", "setup_wan.sh"),
+    CMD_SHELL_NAKD("goonline", "go_online.sh"),
+    CMD_SHELL_NAKD("inetstat", "get_inetstat.sh"),
+    CMD_SHELL_NAKD("nrouting", "toggle_routing.sh"),
+    CMD_SHELL_NAKD("wlaninfo", "wlan_info.sh"),
+    CMD_SHELL_NAKD("setstage", "set_stage.sh"),
+    CMD_SHELL_NAKD("getstage", "get_stage.sh"),
+    CMD_SHELL_NAKD("stagetor", "toggle_tor.sh"),
+    CMD_SHELL_NAKD("stagevpn", "toggle_vpn.sh"),
+    CMD_SHELL_NAKD("doupdate", "do_update.sh"),
+    CMD_SHELL_NAKD("broadcst", "toggle_broadcast.sh"),
+    CMD_SHELL_NAKD("isportal", "detect_portal.sh"),
 };
 
 command *nakd_get_command(const char *cmd_name) {

inc/shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 #include <json-c/json.h>
+#include "command.h"
 
 #define MAX_SHELL_RESULT_LEN 4096
 
@@ -9,6 +10,25 @@
 
 char *nakd_do_command(const char *script, char **argv);
 json_object *nakd_json_do_command(const char *script, json_object *jcmd);
+
+struct cmd_shell_args {
+    const char *path;
+    const char ***argv;
+};
+
+/* GNU extensions used here */
+#define CMD_SHELL_ARGV(name, path, argv...) \
+    { name, cmd_shell, &(struct cmd_shell_args){ path, \
+                   &(const char*[]){ argv, NULL } } }
+#define CMD_SHELL(name, path) \
+    { name, cmd_shell, &(struct cmd_shell_args){ path, \
+                            &(const char*[]){ NULL } } }
+#define CMD_SHELL_NAKD_ARGV(name, path, argv...) \
+    { name, cmd_shell, &(struct cmd_shell_args){ NAKD_SCRIPT(path), \
+                                &(const char*[]){ argv, NULL } } }
+#define CMD_SHELL_NAKD(name, path) \
+    { name, cmd_shell, &(struct cmd_shell_args){ NAKD_SCRIPT(path), \
+                                         &(const char*[]){ NULL } } }
 
 json_object *cmd_shell(json_object *jcmd, void *priv);
 
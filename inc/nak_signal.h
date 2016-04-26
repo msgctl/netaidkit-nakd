0f322b64853f02755d90f9ea63c7b8c31edb99d8
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-04-26 17:09:13 +0200

    signal: add per-module signal handlers
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- inc/nak_signal.h
+++ inc/nak_signal.h
@@ -1,9 +1,12 @@
 #ifndef NAKD_SIGNAL_H
 #define NAKD_SIGNAL_H
 
+typedef int (*nakd_signal_handler)(int signum);
+
 int nakd_signal_init(void);
 int nakd_signal_cleanup(void);
 
+void nakd_add_handler(nakd_signal_handler handler);
 void nakd_sigwait_loop(void);
 
 #endif
--- nak_signal.c
+++ nak_signal.c
@@ -4,6 +4,11 @@
 #include "log.h"
 #include "thread.h"
 
+struct nakd_signal_handler {
+    nakd_signal_handler impl;
+    struct nakd_signal_handler *next;
+} static *_handlers = NULL;
+
 /* block by default in other threads */
 static const int _sigmask[] = {
     SIGINT,
@@ -44,12 +49,51 @@ static void _set_default_sigmask(void) {
         nakd_terminate("Couldn't set default sigmask");
 }
 
+static struct nakd_signal_handler *_alloc_handler() {
+    struct nakd_signal_handler *ret =
+        malloc(sizeof(struct nakd_signal_handler)); 
+    nakd_assert(ret != NULL);
+
+    ret->impl = NULL;
+    ret->next = NULL;
+    return ret;
+}
+
+void nakd_add_handler(nakd_signal_handler impl) {
+    struct nakd_signal_handler *last_handler;
+
+    if (_handlers == NULL) {
+        last_handler = _handlers = _alloc_handler();
+    } else {
+        for (last_handler = _handlers; last_handler->next != NULL;
+                               last_handler = last_handler->next);
+
+        last_handler = last_handler->next = _alloc_handler();
+    }
+
+    last_handler->impl = impl;
+} 
+
+static void _free_handlers() {
+    struct nakd_signal_handler *handler = _handlers;
+    while (handler != NULL) {
+        struct nakd_signal_handler *next = handler->next;
+        free(handler);
+        handler = next;
+    }
+}
+
 static void _sighandler(int signum) {
-    switch (signum) {
-        default: {
-            nakd_log(L_INFO, "%s caught, terminating.", strsignal(signum));
-            _shutdown = 1;
-        }
+    int handled = 0;
+    for (struct nakd_signal_handler *handler = _handlers;
+         handler->next != NULL; handler = handler->next) {
+        if (!handler->impl(signum))
+            handled = 1;
+    }
+
+    if (!handled) {
+        nakd_log(L_INFO, "%s caught, terminating.", strsignal(signum));
+        _shutdown = 1;
     }
 }
 
@@ -70,5 +114,6 @@ int nakd_signal_init(void) {
 }
 
 int nakd_signal_cleanup(void) {
-    /* noop */
+    _free_handlers();
+    return 0;
 }

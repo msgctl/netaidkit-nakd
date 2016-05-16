a8fc18f4a2d2f95fea3a8fe9ca3c38a50f81b088
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-05-16 20:04:13 +0200

    module: fix dependent module log
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- module.c
+++ module.c
@@ -55,7 +55,7 @@ static void _cleanup_module(struct nakd_module *module) {
         for (const char **depname = (*depm)->deps; *depname; depname++) {
             if (!strcmp(*depname, module->name)) {
                 nakd_log(L_DEBUG, "Cleaning up %s dependent module: %s", module->name,
-                                                                            *depname);
+                                                                       (*depm)->name);
                 _cleanup_module(*depm);
                 continue;
             }

7601bb4aa920a6638f22b53ffffde905d09167a1
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-05-18 22:34:51 +0200

    event: handle events in nakd_wq
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- event.c
+++ event.c
@@ -4,19 +4,12 @@
 #include "log.h"
 #include "misc.h"
 #include "module.h"
+#include "workqueue.h"
 
 #define MAX_EVENTS 32
 #define MAX_EVENT_HANDLERS 32
 
 static pthread_mutex_t _event_mutex;
-static struct nakd_thread *_event_thread;
-static int _event_loop_shutdown;
-
-static pthread_cond_t _event_thread_cv;
-
-/* initialized to EVENT_UNSPECIFIED */
-static enum nakd_event _events[MAX_EVENTS];
-
 static struct event_handler _event_handlers[MAX_EVENT_HANDLERS];
 
 #define EVENT_NAME_ENTRY(event) [event] = #event 
@@ -69,85 +62,41 @@ void nakd_event_remove_handler(struct event_handler *handler) {
     pthread_mutex_unlock(&_event_mutex);
 }
 
-static void __handle_event(enum nakd_event event) {
-    nakd_log(L_DEBUG, "Looking for %s event handlers.", nakd_event_name[event]);
-    for (struct event_handler *handler = _event_handlers;
-         handler < ARRAY_END(_event_handlers); handler++) {
-        if (handler->active && handler->event == event) {
-            nakd_log(L_INFO, "Handling event %s.", nakd_event_name[event]);
-            handler->impl(event, handler->priv);
-        }
-    }
-}
-
-static void __handle_events(void) {
-    nakd_log(L_DEBUG, "Running event handlers.");
-    for (enum nakd_event *ev = _events; ev < ARRAY_END(_events); ev++) {
-        if (*ev != EVENT_UNSPECIFIED) {
-            __handle_event(*ev);
-            *ev = EVENT_UNSPECIFIED;
-        }
-    }
-}
-
-static void _event_loop(struct nakd_thread *thread) {
-    pthread_mutex_lock(&_event_mutex);
-    while (!_event_loop_shutdown) {
-        pthread_cond_wait(&_event_thread_cv, &_event_mutex);
-        __handle_events();
-    }
-    pthread_mutex_unlock(&_event_mutex);
-}
-
-static void _event_loop_shutdown_cb(struct nakd_thread *thread) {
-    _event_loop_shutdown = 1;
-}
-
-static int _create_event_thread(void) {
-    nakd_log(L_DEBUG, "Creating event thread.");
-    if (nakd_thread_create_joinable(_event_loop,
-                        _event_loop_shutdown_cb,
-                        NULL, &_event_thread)) {
-        nakd_log(L_CRIT, "Couldn't create an event thread.");
-        return 1;
-    }
-    return 0;
+static void _call_handler(void *priv) {
+    struct event_handler *handler = priv;
+    handler->impl(handler->event, handler->priv);
 }
 
 void nakd_event_push(enum nakd_event event) {
     pthread_mutex_lock(&_event_mutex);
-    /* only if there's already a handler for this type */
     for (struct event_handler *handler = _event_handlers;
          handler < ARRAY_END(_event_handlers); handler++) {
         if (handler->active && handler->event == event) {
-            for (enum nakd_event *ev = _events; ev < ARRAY_END(_events); ev++) {
-                if (*ev == EVENT_UNSPECIFIED) {
-                    *ev = event;
-                    break;
-                }
-            }
-            break;
+            nakd_log(L_INFO, "Handling event %s.", nakd_event_name[event]);
+
+            struct work_desc _event_desc = {
+                .impl = _call_handler,
+                .name = nakd_event_name[handler->event],
+                .priv = handler
+            };
+            struct work *work = nakd_alloc_work(&_event_desc);
+            nakd_workqueue_add(nakd_wq, work);
         }
     }
-    pthread_cond_signal(&_event_thread_cv);
     pthread_mutex_unlock(&_event_mutex);
 }
-
+    
 static int _event_init(void) {
     pthread_mutex_init(&_event_mutex, NULL);
-    pthread_cond_init(&_event_thread_cv, NULL);
-    _create_event_thread();
 }
 
 static int _event_cleanup(void) {
-    nakd_thread_kill(_event_thread);
-    pthread_cond_destroy(&_event_thread_cv);
     pthread_mutex_destroy(&_event_mutex);
 }
 
 static struct nakd_module module_event = {
     .name = "event",
-    .deps = (const char *[]){ "thread", NULL },
+    .deps = (const char *[]){ "thread", "workqueue", NULL },
     .init = _event_init,
     .cleanup = _event_cleanup
 };
